0ca8b142bac98deec944716e4a7f4ced45abf836
Roel Walraven <mail@cytodev.io>
2019-01-03 22:45:10 +0100

    Add PullRequestBranchEntity

---
+++ src/entities/PullRequestBranchEntity.php
@@ -0,0 +1,54 @@
+<?php
+
+    namespace cytodev\integration\bitbucket\webhooks\entities;
+
+    use \ReflectionException;
+    use \LogicException;
+
+    use cytodev\integration\bitbucket\webhooks\autological\Entity;
+
+    /**
+     * <h1>Class PullRequestBranchEntity</h1>
+     *   This entity is subject to change whenever Atlassian feels like changing their request payloads
+     *
+     * @package cytodev\integration\bitbucket\webhooks\entities
+     *
+     * @method string           getBranch
+     * @method string           getCommit
+     * @method RepositoryEntity getRepository
+     */
+    class PullRequestBranchEntity extends Entity {
+
+        /**
+         * @var string
+         */
+        private $branch;
+
+        /**
+         * @var string
+         */
+        private $commit;
+
+        /**
+         * @var RepositoryEntity
+         */
+        private $repository;
+
+        /**
+         * <h2>PullRequestBranchEntity constructor.</h2>
+         *
+         * @param array $payload The payload as sent by Bitbucket [Defaults: empty]
+         *
+         * @throws ReflectionException When ReflectionClass cannot find the calling class
+         * @throws LogicException      When a private field has no default and is not set by Bitbucket's payload
+         */
+        public function __construct(array $payload = []) {
+            $this->repository = isset($payload["repository"]) && is_array($payload["repository"]) ? new RepositoryEntity($payload["repository"]) : null;
+
+            $this->branch = isset($payload["branch"]) && isset($payload["branch"]["name"]) ? $payload["branch"]["name"] : null;
+            $this->commit = isset($payload["commit"]) && isset($payload["commit"]["hash"]) ? $payload["commit"]["hash"] : null;
+
+            $this->validateConstruction();
+        }
+
+    }
