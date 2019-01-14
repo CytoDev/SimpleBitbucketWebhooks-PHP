c8fc486768eb2141df7916123ff67f04a789eddf
Roel Walraven <mail@cytodev.io>
2019-01-14 20:41:23 +0100

    Add __call method to autological Entity

--- src/autological/Entity.php
+++ src/autological/Entity.php
@@ -37,6 +37,38 @@
         }
 
         /**
+         * <h2>__call</h2>
+         *   Wrapper method to allow getProperty() and setProperty() syntax in child classes
+         *
+         * @param string $name      Method name
+         * @param array  $arguments Arguments to call the method with
+         *
+         * @throws BadMethodCallException When implied property does not exist
+         * @throws IllegalMethodException When a call to setProperty() is made
+         * @throws BadMethodCallException When called method does not exist
+         *
+         * @return mixed
+         */
+        final public function __call(string $name, array $arguments) {
+            $property = lcfirst(substr($name, 3, strlen($name)));
+
+            switch(substr($name, 0, 3)) {
+                case "get":
+                    if(!property_exists($this, $property))
+                        throw new BadMethodCallException("Property \"{$property}\" not found in \"" . get_class($this) . "\"");
+
+                    return $this->__get($property);
+                case "set";
+                    return $this->__set($property, $arguments[0]);
+                default:
+                    if(!method_exists($this, $name))
+                        throw new BadMethodCallException("Method \"{$name}\" not found in \"" . get_class($this) . "\"");
+
+                    return $this->$name($arguments);
+            }
+        }
+
+        /**
          * <h2>Magic __get</h2>
          *   Gets hidden properties inside a child class
          *
