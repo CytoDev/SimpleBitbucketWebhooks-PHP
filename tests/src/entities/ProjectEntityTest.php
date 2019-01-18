9d4b366b54c3e9f42c7ed2fc1fb6dde74b0beb8e
Roel Walraven <mail@cytodev.io>
2019-01-18 22:45:53 +0100

    Add ProjectEntityTest tests

--- tests/src/entities/ProjectEntityTest.php
+++ tests/src/entities/ProjectEntityTest.php
@@ -4,8 +4,33 @@
 
     namespace cytodev\integration\bitbucket\webhooks;
 
+    use \LogicException;
+    use \ReflectionException;
+
     use PHPUnit\Framework\TestCase;
 
+    use cytodev\integration\bitbucket\webhooks\entities\ProjectEntity;
+
+
+    /**
+     * <h1>Class ProjectEntityTest</h1>
+     *
+     * @package cytodev\integration\bitbucket\webhooks
+     */
     final class ProjectEntityTest extends TestCase {
+
+        /**
+         * <h2>testCannotInstantiateWithoutData</h2>
+         *
+         * @throws LogicException
+         * @throws ReflectionException
+         *
+         * @return void
+         */
+        public function testCannotInstantiateWithoutData() {
+            $this->expectException(LogicException::class);
+
+            new ProjectEntity();
+        }
 
     }
