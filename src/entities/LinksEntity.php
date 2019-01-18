2cde5aa14c3e3d9cd6f81f846a89de3a68694db0
Roel Walraven <mail@cytodev.io>
2019-01-18 21:34:01 +0100

    Add additional links to LinksEntity

--- src/entities/LinksEntity.php
+++ src/entities/LinksEntity.php
@@ -13,16 +13,54 @@
      *
      * @package cytodev\integration\bitbucket\webhooks\entities
      *
-     * @method string getSelf
-     * @method string getHtml
-     * @method string getAvatar
+     * @method string|null getActivity
+     * @method string|null getApprove
+     * @method string|null getAvatar
+     * @method string|null getComments
+     * @method string|null getCommits
+     * @method string|null getDecline
+     * @method string|null getDiff
+     * @method string      getHtml
+     * @method string|null getMerge
+     * @method string|null getSelf
+     * @method string|null getStatuses
      */
     class LinksEntity extends Entity {
 
         /**
          * @var string|null
          */
-        protected $self;
+        protected $activity;
+
+        /**
+         * @var string|null
+         */
+        protected $approve;
+
+        /**
+         * @var string|null
+         */
+        protected $avatar;
+
+        /**
+         * @var string|null
+         */
+        protected $comments;
+
+        /**
+         * @var string|null
+         */
+        protected $commits;
+
+        /**
+         * @var string|null
+         */
+        protected $decline;
+
+        /**
+         * @var string|null
+         */
+        protected $diff;
 
         /**
          * @var string
@@ -32,7 +70,17 @@
         /**
          * @var string|null
          */
-        protected $avatar;
+        protected $merge;
+
+        /**
+         * @var string|null
+         */
+        protected $self;
+
+        /**
+         * @var string|null
+         */
+        protected $statuses;
 
         /**
          * <h2>LinksEntity constructor.</h2>
@@ -43,9 +91,17 @@
          * @throws LogicException      When a private field has no default and is not set by Bitbucket's payload
          */
         public function __construct(array $payload = []) {
-            $this->self   = isset($payload["self"])   && isset($payload["self"]["href"])   ? $payload["self"]["href"]   : null;
-            $this->html   = isset($payload["html"])   && isset($payload["html"]["href"])   ? $payload["html"]["href"]   : null;
-            $this->avatar = isset($payload["avatar"]) && isset($payload["avatar"]["href"]) ? $payload["avatar"]["href"] : null;
+            $this->activity = isset($payload["activity"]) && isset($payload["activity"]["href"]) ? $payload["activity"]["href"] : null;
+            $this->approve  = isset($payload["approve"])  && isset($payload["approve"]["href"])  ? $payload["approve"]["href"]  : null;
+            $this->avatar   = isset($payload["avatar"])   && isset($payload["avatar"]["href"])   ? $payload["avatar"]["href"]   : null;
+            $this->comments = isset($payload["comments"]) && isset($payload["comments"]["href"]) ? $payload["comments"]["href"] : null;
+            $this->commits  = isset($payload["commits"])  && isset($payload["commits"]["href"])  ? $payload["commits"]["href"]  : null;
+            $this->decline  = isset($payload["decline"])  && isset($payload["decline"]["href"])  ? $payload["decline"]["href"]  : null;
+            $this->diff     = isset($payload["diff"])     && isset($payload["diff"]["href"])     ? $payload["diff"]["href"]     : null;
+            $this->html     = isset($payload["html"])     && isset($payload["html"]["href"])     ? $payload["html"]["href"]     : null;
+            $this->merge    = isset($payload["merge"])    && isset($payload["merge"]["href"])    ? $payload["merge"]["href"]    : null;
+            $this->self     = isset($payload["self"])     && isset($payload["self"]["href"])     ? $payload["self"]["href"]     : null;
+            $this->statuses = isset($payload["statuses"]) && isset($payload["statuses"]["href"]) ? $payload["statuses"]["href"] : null;
 
             $this->validateConstruction();
         }
